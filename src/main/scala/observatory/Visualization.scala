ab870ca2e754b474ede415bddf398f606bdbf7ce
AIgnatiev <aignatiev@luxoft.com>
2017-08-06 10:21:12 +0300

    2 milestone needs to be fixed

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import java.lang.Math.{abs, acos, cos, sin}
+
 import com.sksamuel.scrimage.{Image, Pixel}
 
 /**
@@ -9,29 +11,71 @@ object Visualization {
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param location     Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
-  def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+  def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double =
+    temperatures.par.find { case (loc, _) => distance(location, loc) < 1 }.map(_._2).getOrElse {
+      temperatures.par.map { case (loc, temp) => temp / squared(distance(location, loc)) }.sum /
+        temperatures.par.map { case (loc, _) => 1 / squared(distance(location, loc)) }.sum
+    }
+
+  private val EARTH_RADIUS_KM = 6371
+
+  private def squared(d: Double) = d * d
+
+  private def distance(p: Location, q: Location): Double = {
+    val (fi1, lambda1) = (p.lat, p.lon)
+    val (fi2, lambda2) = (q.lat, q.lon)
+    abs(
+      acos(sin(fi1) * sin(fi2) + cos(fi1) * cos(fi2) * cos(lambda1 - lambda2)) * EARTH_RADIUS_KM
+    )
   }
 
   /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+    val sorted = points.toSeq.sortBy(_._1)
+    val lowerPoint = sorted.filter(_._1 <= value).lastOption
+    val upperPoint = sorted.find(_._1 >= value)
+
+    /*lowerPoint.map(l => upperPoint.map(u => linerp(value, l, u)).getOrElse(l._2))
+      .getOrElse(upperPoint.get._2)*/
+
+    lowerPoint match {
+      case None => sorted.head._2
+      case Some(lower) =>
+        upperPoint match {
+          case None => sorted.last._2
+          case Some(upper) => linerp(value, lower, upper)
+        }
+    }
+  }
+
+  private def linerp(value: Double, lower: (Double, Color), upper: (Double, Color)): Color = {
+    if (lower == upper) return lower._2
+    val (x0, y0) = lower
+    val (x1, y1) = upper
+    val red = (y0.red * (x1 - value) + y1.red * (value - x0)) / (x1 - x0)
+    val green = (y0.green * (x1 - value) + y1.green * (value - x0)) / (x1 - x0)
+    val blue = (y0.blue * (x1 - value) + y1.blue * (value - x0)) / (x1 - x0)
+    Color(red.round.toInt, green.round.toInt, blue.round.toInt)
   }
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
+    * @param colors       Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
-    ???
+    val colorMap = for {
+      lat <- -180 to 180
+      lon <- 90 to -90 by -1
+    } yield interpolateColor(colors, predictTemperature(temperatures, Location(lat, lon)))
+    Image(360, 180, colorMap.map(color => Pixel(color.red, color.green, color.blue, 255)).toArray)
   }
 
 }
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -1,10 +1,55 @@
 package observatory
 
 
-import org.scalatest.FunSuite
 import org.scalatest.prop.Checkers
+import org.scalatest.{FunSuite, Matchers}
 
-trait VisualizationTest extends FunSuite with Checkers {
+trait VisualizationTest extends FunSuite with Checkers with Matchers {
+  test("interpolate color boundaries") {
+    val colors = Seq((60d, Color(255, 255, 255)), (32d, Color(255, 0, 0)))
+    Visualization.interpolateColor(
+      colors,
+      60
+    ) shouldBe Color(255, 255, 255)
 
+    Visualization.interpolateColor(
+      colors,
+      32
+    ) shouldBe Color(255, 0, 0)
 
+    Visualization.interpolateColor(
+      colors,
+      30
+    ) shouldBe Color(255, 0, 0)
+
+    Visualization.interpolateColor(
+      colors,
+      70
+    ) shouldBe Color(255, 255, 255)
+  }
+
+  test("interpolate in between") {
+    val colors = Seq((20d, Color(255, 200, 200)), (10d, Color(255, 0, 0)), (30d, Color(0, 0, 0)))
+    Visualization.interpolateColor(
+      colors,
+      15
+    ) shouldBe Color(255, 100, 100)
+
+    Visualization.interpolateColor(
+      colors,
+      19
+    ) shouldBe Color(255, 180, 180)
+
+    Visualization.interpolateColor(
+      colors,
+      11
+    ) shouldBe Color(255, 20, 20)
+  }
+
+  test("interpolate colors negative") {
+    Visualization.interpolateColor(
+      Seq((-20d, Color(255, 200, 200)), (-10d, Color(255, 0, 0)), (-30d, Color(0, 0, 0))),
+      -11
+    ) shouldBe Color(255, 20, 20)
+  }
 }
